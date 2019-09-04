4fe3fa0bd77ef835eeffe7409149e0d72ff6bfa8
ishiokamika <winter.buy.1209@outlook.jp>
2019-09-04 19:29:37 +0900

    comitt gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   root 'groups#index'
-  resources :users, only: [:index, :edit, :update] do
+  resources :users, only: [:index, :edit, :update,] do
     collection do
       get 'search'
     end
